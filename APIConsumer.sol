fab249bef6cdf5a5b305722588511f22443d5338
Madhukar Gaida <86058993+madhukar123456@users.noreply.github.com>
2022-05-20 09:08:55 +0545

    Update APIConsumer.sol

--- APIConsumer.sol
+++ APIConsumer.sol
@@ -7,7 +7,7 @@ import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 contract APIConsumer is ChainlinkClient {
 
 
-uint256 public distance;
+uint256 public _endOdometer;
 
 address private oracle;
 bytes32 private jobId;
@@ -15,8 +15,8 @@ uint256 private fee;
 
 /**
  * Network: Kovan
- * Oracle: 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e
- * Job ID: 29fa9aa13bf1468788b7cc4a500a45b8
+ * Oracle: 0xA54651f8d131CD42b225e0b3E5ECc36F5d763946
+ * Job ID: "26f8ddbb8d5e4be88f4ee943379a7539"
  * Fee: 0.1 LINK
  */
 constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) public {
@@ -25,9 +25,9 @@ constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) 
     } else {
         setChainlinkToken(_link);
     }
-    // oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
-    // jobId = "29fa9aa13bf1468788b7cc4a500a45b8";
-    // fee = 0.1 * 10 ** 18; // 0.1 LINK
+    // oracle = 0xA54651f8d131CD42b225e0b3E5ECc36F5d763946;
+    // jobId = "26f8ddbb8d5e4be88f4ee943379a7539";
+    // fee = 1000000000000000000
     oracle = _oracle;
     jobId = stringToBytes32(_jobId);
     fee = _fee;
@@ -40,17 +40,6 @@ constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) 
 function requestVolumeData(string memory _vehicleId, string memory _encToken) public returns (bytes32 requestId) 
 {
     Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-    
-    // Set the path to find the desired data in the API response, where the response format is:
-    // {"RAW":
-    //   {"ETH":
-    //    {"USD":
-    //     {
-    //      "VOLUME24HOUR": xxx.xxx,
-    //     }
-    //    }
-    //   }
-    //  }
     request.add("vehicleId", _vehicleId);
     request.add("encToken", _encToken);
 
@@ -64,7 +53,7 @@ function requestVolumeData(string memory _vehicleId, string memory _encToken) pu
  */ 
 function fulfill(bytes32 _requestId, uint256 _distance) public recordChainlinkFulfillment(_requestId)
 {
-    distance = _distance;
+    _endOdometer = _distance;
 }
 
 function stringToBytes32(string memory source) public pure returns (bytes32 result) {
