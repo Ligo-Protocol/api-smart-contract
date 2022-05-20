797c86b634f157b7e7b8fea7ba5e6ab0c8bd6f8c
Madhukar Gaida <86058993+madhukar123456@users.noreply.github.com>
2022-05-20 14:18:55 +0545

    Update APIConsumer.sol

--- APIConsumer.sol
+++ APIConsumer.sol
@@ -9,28 +9,28 @@ contract APIConsumer is ChainlinkClient {
 
 uint256 public _endOdometer;
 
+address private link;
 address private oracle;
 bytes32 private jobId;
 uint256 private fee;
 
 /**
  * Network: Kovan
- * Oracle: 0xA54651f8d131CD42b225e0b3E5ECc36F5d763946
- * Job ID: "26f8ddbb8d5e4be88f4ee943379a7539"
- * Fee: 0.1 LINK
+ * Kovan link address = 0xa36085F69e2889c224210F603D836748e7dC0088
  */
-constructor(address _oracle, string memory _jobId, uint256 _fee, address _link) public {
-    if (_link == address(0)) {
+constructor() public {
+    link = 0xa36085F69e2889c224210F603D836748e7dC0088;
+    if (link == address(0)) {
         setPublicChainlinkToken();
     } else {
-        setChainlinkToken(_link);
+        setChainlinkToken(link);
     }
-    // oracle = 0xA54651f8d131CD42b225e0b3E5ECc36F5d763946;
-    // jobId = "26f8ddbb8d5e4be88f4ee943379a7539";
-    // fee = 1000000000000000000
-    oracle = _oracle;
-    jobId = stringToBytes32(_jobId);
-    fee = _fee;
+    // oracle = 0x7caBbcCAa965F3466387C001c3154BD69cA26AF5;
+    // jobId = "b2779045343f4d21879c33d1d95dca36";
+    // fee = 0
+    oracle = 0x7caBbcCAa965F3466387C001c3154BD69cA26AF5;
+    jobId = stringToBytes32("b2779045343f4d21879c33d1d95dca36");
+    fee = 0;
 }
 
 function requestVolumeData(string memory _vehicleId, string memory _encToken) public returns (bytes32 requestId) 
